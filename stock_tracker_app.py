0e2c4e041223fe4f1390284ce7debe8bfaca6e04
enochk <enochkung@gmail.com>
2021-03-03 22:54:58 +0000

    auto update of stock prices

--- stock_tracker_app.py
+++ stock_tracker_app.py
@@ -1,13 +1,23 @@
 ## stock tracker
 
-from flask import Flask, request, render_template, redirect, url_for
+from flask import Flask, request, render_template, redirect, url_for, jsonify
 from typing import Dict
 import yfinance as yf
+import numpy as np
+from multiprocessing import Value
 
 app = Flask(__name__)
 
 monitored_stock: Dict[str, Dict[str, float]] = dict()
 labels = ["today's opening price: ", "today's high:", "today's low:"]
+monitored_stock = ["AAPL", "HSBC"]
+purchased_stock = dict()
+monitor_stock_price_dict = dict()
+purchased_stock_price_dict = dict()
+
+rando = np.random.rand()
+
+seconds = Value("i", 0)
 
 
 def obtain_stock_info(stock):
@@ -18,32 +28,23 @@ def obtain_stock_info(stock):
 
     hist = hist.sort_index(ascending=False)
     current_open = hist["Open"][0]
+    current_price = hist["Close"][0]
     current_high = hist["High"][0]
     current_low = hist["Low"][0]
 
-    return [current_open, current_high, current_low]
+    return [current_open, current_price, current_high, current_low]
 
 
 @app.route("/")
-# @app.route("/home")
+@app.route("/home")
 def display_home():
-    return render_template("tracker_homepage.html")
-    # return render_template("example_menu.html")
-
-
-@app.route("/to_be_created", methods=["POST"])
-def display_to_be_created():
-    q1 = request.form["q1"]
-    investments.append(0)
-    return render_template("to_be_created.html")
+    # return render_template("tracker_homepage.html")
+    return redirect(url_for("true_monitor"))
 
 
 @app.route("/stock_info", methods=["POST"])
 def display_stock_info():
     stock = request.form["stock"]
-    import pdb
-
-    pdb.set_trace()
     if stock == "":
         stock = "HSBC"
     try:
@@ -56,6 +57,7 @@ def display_stock_info():
 def find_stock_info(stock):
     labels = ["today's opening price: ", "today's high:", "today's low:"]
     info = obtain_stock_info(stock)
+
     return render_template(
         "get_info_page.html",
         stock=stock,
@@ -64,16 +66,69 @@ def find_stock_info(stock):
     )
 
 
+@app.route("/update_stock_info", methods=["POST"])
+def updatestock():
+    ## get new stock info
+    price = np.random.randint(0, 100)
+
+    ## count time spent on page
+    seconds.value += 1
+    return jsonify("", render_template("table_model.html", stock="SUP", price=price))
+
+
 @app.route("/monitor", methods=["POST"])
 def display_monitor():
+    """ NEED CHANGE: will take input and add to monitor stock dict and then run true_monitor """
     stock = request.form["stock"]
-
     stock_info = obtain_stock_info(stock)
     monitored_stock[stock] = dict()
     for label_index, label in enumerate(labels):
         monitored_stock[stock][label] = stock_info[label_index]
-
+    # return render_template("sample_table.html", stock="AAPL")
     return render_template("monitored_info_page.html", stocks=monitored_stock)
+
+
+@app.route("/true_monitor")
+def true_monitor():
+    """ display monitor list and purchased list """
+    for key in monitored_stock:
+        [current_open, current_price, current_high, current_low] = obtain_stock_info(
+            key
+        )
+        monitor_stock_price_dict[key] = round(current_price, 4)
+
+    return render_template(
+        "monitor_and_purchase.html", mon_dict=monitor_stock_price_dict
+    )
+
+
+@app.route("/update_monitor", methods=["POST"])
+def update_monitor():
+    for key in monitored_stock:
+        [current_open, current_price, current_high, current_low] = obtain_stock_info(
+            key
+        )
+        monitor_stock_price_dict[key] = round(current_price, 4)
+    return jsonify(
+        "",
+        render_template("update_monitor_table.html", mon_dict=monitor_stock_price_dict),
+    )
+
+
+@app.route("/to_be_created")
+def display_to_be_created():
+    return render_template("to_be_created.html")
+
+
+@app.route("/update_decimal", methods=["POST"])
+def updatedecimal():
+    rando = np.random.rand()
+    return jsonify({"stock": "0", "price": rando})
+
+
+@app.route("/live_data")
+def display_live_data():
+    return render_template("live_data.html", index=rando)
 
 
 if __name__ == "__main__":
