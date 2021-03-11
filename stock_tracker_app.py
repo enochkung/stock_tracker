b984783f1f1c7dc10ed69dd891738b798c14cee4
enochk <enochkung@gmail.com>
2021-03-11 02:31:20 +0000

    add free funds, two top level updates, commission column

--- stock_tracker_app.py
+++ stock_tracker_app.py
@@ -69,39 +69,8 @@ def true_monitor():
         # get stock abbrev.
         new_stock = request.form["stock"]
         # if stock is not already monitored, add to monitored stock list
-        if new_stock not in monitored_stock:
-            monitored_stock.append(new_stock)
-        # get whether buy or sell, and if buy whether in shares or cost
-        action = [
-            key for key in request.form if key != "stock" and request.form[key] != ""
-        ][0]
-
-        if action == "num_shares":
-            # if action is in number of shares, then initiate stock in num_stock_purchased_dict at 0 stocks
-            if new_stock not in num_stock_purchased_dict:
-                num_stock_purchased_dict[new_stock] = 0
-            # add the number of stocks to be purchased to already recorded number, whether 0 or non-0
-            num_stock_purchased_dict[new_stock] += int(request.form[action])
-        elif action == "cost_shares":
-            # if action is in amount of shares by cost, then using current price, translate to number of shares
-            shares_in_cost = float(request.form[action])
-            stock_live_price = stock_info.get_live_price(new_stock)
-            if new_stock not in num_stock_purchased_dict:
-                num_stock_purchased_dict[new_stock] = 0
-            # add the number of stocks to be purchased to already recorded number, whether 0 or non-0
-            num_of_shares = shares_in_cost // stock_live_price
-            num_stock_purchased_dict[new_stock] += int(
-                shares_in_cost // stock_live_price
-            )
-        elif action == "sell_shares":
-            # if action is sell number of shares, remove amount from num_stock_purchased_dict
-            if new_stock in num_stock_purchased_dict:
-                num_shares_sold = int(request.form[action])
-                num_stock_purchased_dict[new_stock] = max(
-                    num_stock_purchased_dict[new_stock] - num_shares_sold, 0
-                )
-
-        purchase_value_dict[new_stock] = 0.0
+        # num_stock_purchased_dict, purchase_value_dict = buy_or_sell_shares()
+        buy_or_sell_shares(new_stock)
 
     elif "mon_stock" in request.form:
         ## if only add to monitor
@@ -110,47 +79,20 @@ def true_monitor():
         ## stock is not yet bought so set number of stock to zero
         num_stock_purchased_dict[new_stock] = 0
 
+    elif "add_free_funds" in request.form:
+        add_funds = request.form["add_free_funds"]
+        top_level_info["free_funds"] += int(add_funds)
+
     ## get current information of the stocks
-    for key in monitored_stock:
-        [current_open, current_price, current_high, current_low] = obtain_stock_info(
-            key
-        )
-        monitor_stock_price_dict[key] = np.round_(current_price, 2)
-        total_value_dict[key] = np.round_(
-            current_price * num_stock_purchased_dict[key], 2
-        )
-        if "stock" in request.form and request.form["stock"] == key:
-            purchase_value_dict[key] += np.round_(
-                current_price * num_stock_purchased_dict[key], 2
-            )
-            top_level_info["free_funds"] -= np.round_(
-                current_price * num_stock_purchased_dict[key], 2
-            )
+    (
+        monitor_stock_price_dict,
+        total_value_dict,
+        purchase_value_dict,
+        value_color_dict,
+    ) = get_current_price()
 
-        if total_value_dict[key] > purchase_value_dict[key]:
-            value_color_dict[key] = "green"
-        elif total_value_dict[key] == purchase_value_dict[key]:
-            value_color_dict[key] = "grey"
-        else:
-            value_color_dict[key] = "red"
+    update_top_level_initial()
 
-    top_level_info["portfolio"] = sum(total_value_dict.values())
-    if top_level_info["invested"] != 0:
-        top_level_info["return"] = (
-            str(
-                round(
-                    (top_level_info["portfolio"] - top_level_info["invested"])
-                    / top_level_info["invested"]
-                    * 100,
-                    2,
-                )
-            )
-            + "%"
-        )
-    else:
-        top_level_info["return"] = "0%"
-
-    top_level_info["invested"] = sum(purchase_value_dict.values())
     return render_template(
         "monitor_and_purchase.html",
         top_level=top_level_info,
@@ -164,42 +106,14 @@ def true_monitor():
 
 @app.route("/update_monitor", methods=["POST"])
 def update_monitor():
+    (
+        monitor_stock_price_dict,
+        total_value_dict,
+        purchase_value_dict,
+        value_color_dict,
+    ) = get_current_price()
 
-    for key in monitored_stock:
-        [current_open, current_price, current_high, current_low] = obtain_stock_info(
-            key
-        )
-        monitor_stock_price_dict[key] = np.round_(current_price, 2)
-        total_value_dict[key] = np.round_(
-            current_price * num_stock_purchased_dict[key], 2
-        )
-        if "stock" in request.form and request.form["stock"] == key:
-            purchase_value_dict[key] += current_price * num_stock_purchased_dict[key]
-
-        if total_value_dict[key] > purchase_value_dict[key]:
-            value_color_dict[key] = "green"
-        elif total_value_dict[key] == purchase_value_dict[key]:
-            value_color_dict[key] = "grey"
-        else:
-            value_color_dict[key] = "red"
-
-    top_level_info["portfolio"] = sum(total_value_dict.values())
-    if top_level_info["invested"] != 0:
-        top_level_info["return"] = (
-            str(
-                round(
-                    (top_level_info["portfolio"] - top_level_info["invested"])
-                    / top_level_info["invested"]
-                    * 100,
-                    2,
-                )
-            )
-            + "%"
-        )
-    else:
-        top_level_info["return"] = "0%"
-
-    top_level_info["invested"] = sum(purchase_value_dict.values())
+    update_top_level_portfolio()
 
     return jsonify(
         "",
@@ -220,20 +134,117 @@ def display_to_be_created():
     return render_template("to_be_created.html")
 
 
-# @app.route("/live_data")
-# def display_live_data():
-#     return render_template("live_data.html", index=rando)
+def set_colors(total_value, purchase_value):
 
-# @app.route("/monitor", methods=["POST"])
-# def display_monitor():
-#     """ NEED CHANGE: will take input and add to monitor stock dict and then run true_monitor """
-#     stock = request.form["stock"]
-#     stock_info = obtain_stock_info(stock)
-#     monitored_stock[stock] = dict()
-#     for label_index, label in enumerate(labels):
-#         monitored_stock[stock][label] = stock_info[label_index]
-#     # return render_template("sample_table.html", stock="AAPL")
-#     return render_template("monitored_info_page.html", stocks=monitored_stock)
+    if total_value > np.round_(purchase_value, 2):
+        return "green"
+    elif total_value == np.round_(purchase_value, 2):
+        return "grey"
+    else:
+        return "red"
+
+
+def buy_or_sell_shares(new_stock):
+    # if stock is not already monitored, add to monitored stock list
+    if new_stock not in monitored_stock:
+        monitored_stock.append(new_stock)
+    # get whether buy or sell, and if buy whether in shares or cost
+    action = [
+        key for key in request.form if key != "stock" and request.form[key] != ""
+    ][0]
+
+    if action == "num_shares":
+        # if action is in number of shares, then initiate stock in num_stock_purchased_dict at 0 stocks
+        if new_stock not in num_stock_purchased_dict:
+            num_stock_purchased_dict[new_stock] = 0
+        # add the number of stocks to be purchased to already recorded number, whether 0 or non-0
+        num_stock_purchased_dict[new_stock] += int(request.form[action])
+    elif action == "cost_shares":
+        # if action is in amount of shares by cost, then using current price, translate to number of shares
+        shares_in_cost = float(request.form[action])
+        stock_live_price = stock_info.get_live_price(new_stock)
+        if new_stock not in num_stock_purchased_dict:
+            num_stock_purchased_dict[new_stock] = 0
+        # add the number of stocks to be purchased to already recorded number, whether 0 or non-0
+        num_of_shares = shares_in_cost // stock_live_price
+        num_stock_purchased_dict[new_stock] += int(shares_in_cost // stock_live_price)
+    elif action == "sell_shares":
+        # if action is sell number of shares, remove amount from num_stock_purchased_dict
+        if new_stock in num_stock_purchased_dict:
+            num_shares_sold = int(request.form[action])
+            num_stock_purchased_dict[new_stock] = max(
+                num_stock_purchased_dict[new_stock] - num_shares_sold, 0
+            )
+
+    purchase_value_dict[new_stock] = 0.0
+
+    return num_stock_purchased_dict, purchase_value_dict
+
+
+def get_current_price():
+    for key in monitored_stock:
+        [current_open, current_price, current_high, current_low] = obtain_stock_info(
+            key
+        )
+        monitor_stock_price_dict[key] = np.round_(current_price, 2)
+        total_value_dict[key] = np.round_(
+            current_price * num_stock_purchased_dict[key], 2
+        )
+        if "stock" in request.form and request.form["stock"] == key:
+            purchase_value_dict[key] += current_price * num_stock_purchased_dict[key]
+
+        value_color_dict[key] = set_colors(
+            total_value_dict[key], purchase_value_dict[key]
+        )
+    return (
+        monitor_stock_price_dict,
+        total_value_dict,
+        purchase_value_dict,
+        value_color_dict,
+    )
+
+
+def auto_buy_sell():
+    pass
+
+
+def update_top_level_initial():
+    top_level_info["portfolio"] = sum(total_value_dict.values())
+    if top_level_info["invested"] != 0:
+        top_level_info["return"] = (
+            str(
+                round(
+                    (top_level_info["portfolio"] - top_level_info["invested"])
+                    / top_level_info["invested"]
+                    * 100,
+                    2,
+                )
+            )
+            + "%"
+        )
+    else:
+        top_level_info["return"] = "0%"
+
+    top_level_info["invested"] = np.round_(sum(purchase_value_dict.values()), 2)
+    top_level_info["free_funds"] -= top_level_info["invested"]
+
+
+def update_top_level_portfolio():
+    top_level_info["portfolio"] = sum(total_value_dict.values())
+    if top_level_info["invested"] != 0:
+        top_level_info["return"] = (
+            str(
+                round(
+                    (top_level_info["portfolio"] - top_level_info["invested"])
+                    / top_level_info["invested"]
+                    * 100,
+                    2,
+                )
+            )
+            + "%"
+        )
+    else:
+        top_level_info["return"] = "0%"
 
 
 if __name__ == "__main__":
